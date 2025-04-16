08c61070cde7d14fde18bd148eeca3b8dd373354
GANUGA TAGORE <tagore8661@gmail.com>
2025-04-16 18:41:11 +0530

    Testing

--- 17-delete-old-logs.sh
+++ 17-delete-old-logs.sh
@@ -5,21 +5,15 @@
 
 SOURCE_DIR="/home/ec2-user/app-logs"
 
-LOGS_FOLDER="/var/log/shellscript-logs"
-LOG_FILE=$(echo $0 | cut -d "." -f1 )
-TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
-LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
-
 mkdir -p $SOURCE_DIR
-mkdir -p $LOGS_FOLDER
-echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
+echo "Script started executing .." 
 
 FILES_TO_DELETE=$(find $SOURCE_DIR -name "*.log" -mtime +10)
 echo "Files to be deleted: $FILES_TO_DELETE"
 
 while read -r filepath # here filepath is the variable name
 do
-    echo "Deleting file: $filepath" &>>$LOG_FILE_NAME
+    echo "Deleting file: $filepath"
     rm -rf $filepath
     echo "Deleted file: $filepath"
 done <<< $FILES_TO_DELETE  # <<< = it provides a string or variable as input
