6ffda8914fbce0ff8f094aafa6f2f6612c6356cf
GANUGA TAGORE <tagore8661@gmail.com>
2025-04-27 10:13:52 +0530

    docker install

---
+++ install-docker.sh
@@ -0,0 +1,105 @@
+#!/bin/bash
+
+USERID=$(id -u) 
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo "$2 ... FAILURE"
+        exit 1
+    else
+        echo "$2 ... SUCCESS"
+    fi
+}
+
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo "ERROR:: You must have sudo access to execute this script"
+        exit 1
+    fi
+}
+
+CHECK_ROOT
+
+for package in docker
+do
+    dnf list installed docker
+    if [ $? -ne 0 ]
+    then
+        dnf -y install dnf-plugins-core
+        dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+        dnf install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+        
+        VALIDATE $? "Installing docker"
+    else
+        echo -e "docker is already... INSTALLED"
+    fi
+done
+
+# Start and enable Docker service
+systemctl start docker
+VALIDATE $? "Starting Docker service"
+
+systemctl enable docker
+VALIDATE $? "Enabling Docker service"
+
+# Add ec2-user to the Docker group
+usermod -aG docker ec2-user
+VALIDATE $? "Adding ec2-user to Docker group"
+
+
+#!/bin/bash
+
+USERID=$(id -u) 
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo "$2 ... FAILURE"
+        exit 1
+    else
+        echo "$2 ... SUCCESS"
+    fi
+}
+
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo "ERROR:: You must have sudo access to execute this script"
+        exit 1
+    fi
+}
+
+CHECK_ROOT
+
+dnf -y install dnf-plugins-core
+dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+
+for package in docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+do
+    dnf list installed "$package"
+    if [ $? -ne 0 ]
+    then
+        dnf install -y "$package"
+        VALIDATE $? "Installing $package"
+    else
+        echo -e "$package is already... INSTALLED"
+    fi
+done
+
+# Start and enable Docker service
+systemctl start docker
+VALIDATE $? "Starting Docker service"
+
+systemctl enable docker
+VALIDATE $? "Enabling Docker service"
+
+# Add ec2-user to the Docker group
+usermod -aG docker ec2-user
+VALIDATE $? "Adding ec2-user to Docker group"
+
+# Apply group changes immediately
+newgrp docker
+
+echo "Installation complete! If 'ec2-user' still can't run Docker, log out and log back in."
